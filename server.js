7ccc2bc6f5216cc579fcbf5ebb5d700e70bbe1f6
Gabriel Sousa <79923219+Gabriel-Sousa@users.noreply.github.com>
2022-07-31 22:30:41 -0300

    .

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const server = express()
+
 const data = require('./db.json')
 
-server.get('/', (req, res) => {
-  return res.send(Object.values(data));
+server.get('/coffee', (req, res) => {
+  return res.json(data);
 })
 
 server.listen(3333, () => { console.log('Server running') })
