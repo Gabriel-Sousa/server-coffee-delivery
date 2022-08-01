9402f9b991dc739f878ce7ad3602bf54e7df7567
Gabriel Sousa <79923219+Gabriel-Sousa@users.noreply.github.com>
2022-08-01 15:52:34 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import data from '../db.json'
 
 const app = express()
-const port = 3333
+const port = process.env.PORT || 3333
 
 app.get('/coffee', async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
@@ -10,5 +10,5 @@ app.get('/coffee', async (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Running at port:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
