d61ce85a1179a600ca2d72be75a1e728db593a7a
Gabriel Sousa <79923219+Gabriel-Sousa@users.noreply.github.com>
2022-11-22 20:19:34 -0300

    .

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,6 @@ app.get('/coffee', async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.json(data)
 })
-console.log('legal')
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
